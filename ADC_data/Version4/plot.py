37b1abda45224530e9063933f75797a2f1c5def2
Kevin Chen <kevinchen297@berkeley.edu>
2017-04-25 07:42:56 -0700

    numpy not recognized?

---
+++ ADC_data/Version4/plot.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+
+t, vip, _, _, t1, il, _, vbody  = np.loadtxt("digital_reg_unstable.csv", delimiter=',', skiprows=1,unpack=True)
+
+print(len(t))
+
+plt.figure(1)
+plt.plot(t[3000:6500],vip[3000:6500], label = "V-")
+plt.plot(t[3000:6500],vbody[3000:6500], label = "V_body")
+
+ax = plt.gca()
+ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+ax.ticklabel_format(axis='x',style='sci',scilimits=(-2,2))
+
+plt.xlabel("Time (us)")
+plt.ylabel("V- (V)")
+
+plt.title("V-, input to comparator, Vbody")
+
+plt.legend()
+
+plt.figure(2)
+plt.plot(t[3000:6500],il[3000:6500])
+
+ax = plt.gca()
+ax.ticklabel_format(axis='y',style='sci',scilimits=(-2,2))
+ax.ticklabel_format(axis='x',style='sci',scilimits=(-2,2))
+
+plt.title("Current into PMOS source")
+plt.xlabel("Time (us)")
+plt.ylabel("I_leak (A)")
+
+plt.show()
